4bb0718f4b97c51eb4341313609c51e6679b1c6a
resteve <resteve@zikzakmedia.com>
2015-12-01 09:43:43 +0100

    Tests

--- tests/test_party_manufacturer.py
+++ tests/test_party_manufacturer.py
@@ -1,27 +1,18 @@
-#!/usr/bin/env python
+# This file is part of the party_manufacturer module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
-    'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('party_manufacturer')
-
-    def test0005views(self):
-        'Test views'
-        test_view('party_manufacturer')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class PartyManufacturerTestCase(ModuleTestCase):
+    'Test Party Manufacturer module'
+    module = 'party_manufacturer'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        PartyManufacturerTestCase))
     return suite
